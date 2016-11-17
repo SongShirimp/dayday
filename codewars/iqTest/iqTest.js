315aec0124196d31a42a85399f525ccef9652591
hyeongyu song <themunt3@gmail.com>
2016-11-17 13:50:35 +0900

    1

--- codewars/iqTest/iqTest.js
+++ codewars/iqTest/iqTest.js
@@ -9,7 +9,7 @@ Examples :
 iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
 
 iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd*/
-
+ 
 function iqTest(numbers) {
     var some = numbers.split(' ');
     var oddCount = 0;
