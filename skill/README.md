71ded98b91f863d10b15b2df6ce4ca6bc3876382
hyeongyu song <themunt3@gmail.com>
2016-12-02 13:10:30 +0900

    skill

---
+++ skill/README.md
@@ -0,0 +1,1 @@
+for get advanced programming skills.
---
+++ skill/test.js
@@ -0,0 +1,23 @@
+function fee(nAge) {
+  var nFee = 1000;
+
+  var aRate = [
+    0.2, 0.2, 0.2,  //1~3 : 20%
+    0.5, 0.5, 0.5, 0.5,  //4~7 : 50%
+    0.8, 0.8, 0.8   //7~10 : 80%
+  ];
+
+  console.log("요금표 :");
+  for (var i = 1; i <= 10; i++)
+  {
+    console.log(i + "세 : " + nFee * aRate[i - 1] + "원" );
+  }
+
+  if (nAge < 1) nAge = 1;
+  if (nAge > 10) nAge = 10;
+  console.log("\n최종요금 : " + nFee * aRate[nAge - 1] + "원입니다.");
+}
+
+var age = 5;
+console.log("나이를 입력해주세요. : " + age + "\n");
+fee(age);
